3c568a645859426925a49c6b21488f42eea50007
fmanimashaun <fisayo.animashaun@outlook.com>
2023-11-18 19:34:18 +0100

    reset the TDD workflow with auto test action
    Co-authored-by: Ademola Owoeye <ademolaowoeye07@gmail.com>

--- solver.rb
+++ solver.rb
@@ -1,19 +1,2 @@
 class Solver
-  def factorial(num)
-    raise ArgumentError, 'Input must be a non-negative integer' if num.negative?
-
-    (1..num).reduce(1, :*)
-  end
-
-  def reverse(str)
-    str.reverse
-  end
-
-  def fizzbuzz(num)
-    return 'fizzbuzz' if (num % 3).zero? && (num % 5).zero?
-    return 'fizz' if (num % 3).zero?
-    return 'buzz' if (num % 5).zero?
-
-    num.to_s
-  end
 end
