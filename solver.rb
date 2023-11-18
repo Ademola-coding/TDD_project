885440cd89ff023faa17af4468d243b1d8928bad
fmanimashaun <fisayo.animashaun@outlook.com>
2023-11-18 19:37:09 +0100

    Create the fizzbuzz method for Solver class
    Co-authored-by: Ademola Owoeye <ademolaowoeye07@gmail.com>

--- solver.rb
+++ solver.rb
@@ -7,4 +7,11 @@ class Solver
   def reverse(str)
     str.reverse
   end
+
+  def fizzbuzz(num)
+    return 'fizzbuzz' if num % 15 == 0
+    return 'fizz' if num % 3 == 0
+    return 'buzz' if num % 5 == 0
+    num.to_s
+  end
 end
