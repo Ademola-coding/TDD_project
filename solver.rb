e95f72310888e51c3277b0751ecf3ec88d592fa2
Ademola Owoeye <ademolaowoeye07@gmail.com>
2023-11-18 14:44:37 +0100

    Worked on the fizz function

--- solver.rb
+++ solver.rb
@@ -5,15 +5,15 @@ class Solver
     (1..num).reduce(1, :*)
   end
 
-  def reverse(word)
-    word.reverse
-  end
-
   def fizzbuzz(num)
     return 'fizzbuzz' if (num % 15).zero?
     return 'fizz' if (num % 3).zero?
     return 'buzz' if (num % 5).zero?
     
     num.to_s
+  end
+  
+  def reverse(word)
+    word.reverse
   end
 end
