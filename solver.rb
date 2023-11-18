bbbf9d3d7dbaac7b852b20d4ef6bd0301a37016d
fmanimashaun <fisayo.animashaun@outlook.com>
2023-11-18 14:59:16 +0100

    fix linter error
    Co-authored-by: Ademola Owoeye <ademolaowoeye07@gmail.com>

--- solver.rb
+++ solver.rb
@@ -1,7 +1,7 @@
 class Solver
   def factorial(num)
     raise ArgumentError, 'Input must be a non-negative integer' if num.negative?
-    
+
     (1..num).reduce(1, :*)
   end
 
@@ -9,10 +9,10 @@ class Solver
     return 'fizzbuzz' if (num % 15).zero?
     return 'fizz' if (num % 3).zero?
     return 'buzz' if (num % 5).zero?
-    
+
     num.to_s
   end
-  
+
   def reverse(word)
     word.reverse
   end
