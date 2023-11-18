2244589976034151a7ea241641787b5046f305b8
fmanimashaun <fisayo.animashaun@outlook.com>
2023-11-18 19:38:15 +0100

    Fix linter errors
    Co-authored-by: Ademola Owoeye <ademolaowoeye07@gmail.com>

--- solver.rb
+++ solver.rb
@@ -1,6 +1,7 @@
 class Solver
   def factorial(num)
-    raise ArgumentError if num < 0
+    raise ArgumentError if num.negative?
+
     (1..num).reduce(1, :*)
   end
 
@@ -9,9 +10,10 @@ class Solver
   end
 
   def fizzbuzz(num)
-    return 'fizzbuzz' if num % 15 == 0
-    return 'fizz' if num % 3 == 0
-    return 'buzz' if num % 5 == 0
+    return 'fizzbuzz' if (num % 15).zero?
+    return 'fizz' if (num % 3).zero?
+    return 'buzz' if (num % 5).zero?
+
     num.to_s
   end
 end
