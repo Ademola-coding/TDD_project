7fae60f1c535aea8adbd464418d1551fb58c57c3
fmanimashaun <fisayo.animashaun@outlook.com>
2023-11-18 13:55:06 +0100

    Fix typo error
    Co-authored-by: Ademola Owoeye <ademolaowoeye07@gmail.com>

--- solver.rb
+++ solver.rb
@@ -1,6 +1,6 @@
 class Solver
   def factorial(num)
-    raise ArgumentError, 'Input must be a non-negative integer' if n.negative?
+    raise ArgumentError, 'Input must be a non-negative integer' if num.negative?
 
     (1..num).reduce(1, :*)
   end
