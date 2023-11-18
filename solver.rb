7ba068088260aad7519c87168e6f3d994e6c7146
Ademola Owoeye <ademolaowoeye07@gmail.com>
2023-11-18 14:32:34 +0100

    Update solver.rb

--- solver.rb
+++ solver.rb
@@ -1,7 +1,6 @@
 class Solver
   def factorial(num)
     raise ArgumentError, 'Input must be a non-negative integer' if num.negative?
-
     (1..num).reduce(1, :*)
   end
 
@@ -13,7 +12,6 @@ class Solver
     return 'fizzbuzz' if (num % 15).zero?
     return 'fizz' if (num % 3).zero?
     return 'buzz' if (num % 5).zero?
-
     num.to_s
   end
 end
