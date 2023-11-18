05518e812e296d43410ddcac08daa73bf34067d3
fmanimashaun <fisayo.animashaun@outlook.com>
2023-11-18 19:30:24 +0100

    the #fizzbuzz for divisible by 5 test passed
    Co-authored-by: Ademola Owoeye <ademolaowoeye07@gmail.com>

--- solver.rb
+++ solver.rb
@@ -12,5 +12,6 @@ class Solver
   def fizzbuzz(num)
     return 'fizzbuzz' if (num % 3).zero? && (num % 5).zero?
     return 'fizz' if (num % 3).zero?
+    return 'buzz' if (num % 5).zero?
   end
 end
